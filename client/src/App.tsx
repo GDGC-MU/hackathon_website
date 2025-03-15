596e1943b5698f9116e52493a3be7eea66af0d04
RM25BAM <nmpiedrabuena2003@gmail.com>
2025-03-15 18:27:18 -0400

    added tailwindcss- was giving error before

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,18 +1,11 @@
 import './App.css'
-import Header from './components/header'
-import Hero from './components/hero'
-import Footer from './components/footer'
+
 function App() {
-  /* const [count, setCount] = useState(0) */
 
   return (
-    <>
-      <div className="bg-amber-300">
-        <Header />
-        <Hero />
-        <Footer />
-      </div>
-    </>
+   <div>
+    
+   </div>
   )
 }
 
